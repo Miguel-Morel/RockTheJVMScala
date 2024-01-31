6ddf933817e892dc36a1a7a5646402e742dba4cf
Miguel Morel <89816412+mmorel1@users.noreply.github.com>
2024-01-30 22:15:25 -0500

    adding method notation exercises

--- courses/scala-2-beginner/scala-2-beginners/src/lectures/part2oop/MethodNotations.scala
+++ courses/scala-2-beginner/scala-2-beginners/src/lectures/part2oop/MethodNotations.scala
@@ -4,12 +4,17 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
+    def learns(something: String): String = s"$name learns $something"
+    def learnsScala = this learns("Scala")
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched Inception $n times"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -46,4 +51,52 @@ object MethodNotations extends App {
   println(mary.apply())
   println(mary()) //equivalent
 
+  /*
+    1. overload the + operator
+       ex. mary + "the rockstar" => new person "Mary (the rockstar)"
+
+    2. add an age to the Person class (val = 0)
+       add a unary + operator => new person with the age + 1 (similar to ++ in java/c++)
+
+    3. add a "learns" method in the Person class => "Mary learns Scala"
+       add a learnsScala mehtod, calls learns method with "Scala"
+       use it in postfix notation
+
+    4. overload the apply method
+       ex. mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+
+  /*
+  1. overload the + operator
+         ex. mary + "the rockstar" => new person "Mary (the rockstar)"
+   */
+
+  println((mary + "the rockstar")())
+  println((mary + "the rockstar").apply()) //equivalent to above
+
+  /*
+  2. add an age to the Person class (val = 0)
+     add a unary + operator => new person with the age + 1 (similar to ++ in java/c++)
+   */
+  println((+mary).age)  //1
+
+  /*
+  3. add a "learns" method in the Person class => "Mary learns Scala"
+     add a learnsScala mehtod, calls learns method with "Scala"
+     use it in postfix notation
+   */
+  println(mary learnsScala)
+
+  /*
+  4. overload the apply method
+     ex. mary.apply(2) => "Mary watched Inception 2 times"
+   */
+  println(mary.apply(2))
+  println(mary(2)) //equivalent
+
+
 }
+
+
+
