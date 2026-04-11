49927348db327711a04957560acc2e573405edd3
Miguel Morel <memc22@gmail.com>
2026-04-11 08:28:54 -0400

    functional set pt2

--- courses/scala-2-advanced/src/exercises/MySet.scala
+++ courses/scala-2-advanced/src/exercises/MySet.scala
@@ -15,13 +15,30 @@ trait MySet[A] extends (A => Boolean) {
 
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
-  def ++(anotherSet: MySet[A]): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A] // union
 
   def map[B](f: A => B): MySet[B]
   def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
 
+  /*
+    Exercise
+    - removing an element
+    - difference with another set
+    - intersection with another set
+   */
+
+  def -(elem: A): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A] // difference
+  def &(anotherSet: MySet[A]): MySet[A] // intersection
+
+  /*
+    Exercise
+    implement a unary_! = negation of a set
+    set[1,2,3] =>
+   */
+  def unary_! : MySet[A]
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -33,6 +50,45 @@ class EmptySet[A] extends MySet[A] {
   def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
   def filter(predicate: A => Boolean): MySet[A] = this
   def foreach(f: A => Unit): Unit = ()
+
+  //pt2
+  def -(elem: A): MySet[A] = this
+  def --(anotherSet: MySet[A]): MySet[A] = this
+  def &(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new AllInclusiveSet[A]
+
+}
+
+class AllInclusiveSet[A] extends MySet[A] {
+  override def contains(elem: A): Boolean = true
+
+  override def +(elem: A): MySet[A] = this
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = this
+
+  // naturals = allInclusiveSet[Int] = all the natural numbers
+  // naturals.map(x => x % 3) => ???
+  // [0 1 2]
+  override def map[B](f: A => B): MySet[B] = ???
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+
+  override def filter(predicate: A => Boolean): MySet[A] = ??? // property based set
+
+  override def foreach(f: A => Unit): Unit = ???
+
+  override def -(elem: A): MySet[A] = ???
+
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new EmptySet[A]
+}
+
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -67,6 +123,15 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail foreach f
   }
+
+  //pt2
+  def -(elem: A): MySet[A] =
+    if(head == elem) tail
+    else tail - elem + head
+
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet) // intersection = filtering
+
 }
 
 object MySet {
