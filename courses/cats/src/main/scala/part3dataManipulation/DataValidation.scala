b289cdac182be9f75b6ac080618e817c545962a9
Miguel Morel <memc22@gmail.com>
2026-07-28 02:37:43 -0400

    data validation pt2

--- courses/cats/src/main/scala/part3dataManipulation/DataValidation.scala
+++ courses/cats/src/main/scala/part3dataManipulation/DataValidation.scala
@@ -3,6 +3,7 @@ package part3dataManipulation
 import cats.Semigroup
 
 import scala.annotation.tailrec
+import scala.util.Try
 
 object DataValidation {
 
@@ -21,7 +22,7 @@ object DataValidation {
     else tailRecPrime(Math.abs(n / 2))
   }
 
-  // todo: use Either
+  // todo 1: use Either
 
   /*
     - n must be a prime
@@ -48,8 +49,75 @@ object DataValidation {
       .combine(Validated.cond(n < 100, n, List("number must be less than or equal to 100")))
       .combine(Validated.cond(testPrime(n), n, List("number must be prime")))
 
+  // chain
+  aValidValue.andThen(_ => anInvalidValue)
+
+  // test a valid value
+  aValidValue.ensure(List("something went wrong"))(_ % 2 == 0)
+
+  // transform
+  aValidValue.map(_ + 1)
+  aValidValue.leftMap(_.length)
+  aValidValue.bimap(_.length, _ + 1)
+
+  // interoperate with std lib
+  val eitherToValidated: Validated[List[String], Int] = Validated.fromEither(Right(42))
+  val optionToValidated: Validated[List[String], Int] = Validated.fromOption(None, List("nothing present here"))
+  val tryToValidated: Validated[Throwable, Int] = Validated.fromTry(Try("something".toInt))
+
+  // backwards
+  aValidValue.toOption
+  aValidValue.toEither
+
+  // todo 2: form validation
+  object FormValidation {
+    import cats.instances.string._
+    type FormValidation[T] = Validated[List[String], T]
+
+    def getValue(form: Map[String, String], fieldName: String): FormValidation[String] =
+      Validated.fromOption(form.get(fieldName), List(s"the field $fieldName must be specified"))
+
+    def nonBlank(value: String, fieldName: String): FormValidation[String] =
+      Validated.cond(value.length > 0, value, List(s"the field $fieldName must not be blank"))
+
+    def emailProperForm(email: String): FormValidation[String] =
+      Validated.cond(email.contains("@"), email, List("email is invalid"))
+
+    def passwordCheck(password: String): FormValidation[String] =
+      Validated.cond(password.length >= 10, password, List("passwords must be at least 10 characters long"))
+    /*
+      fields:
+      - name
+      - email
+      - password
+
+      rules:
+      - above MUST be specified
+      - name must not be blank
+      - email must have "@"
+      - password must have >= 10 characters
+     */
+
+    def validateForm(form: Map[String, String]): FormValidation[String] =
+      getValue(form, "name").andThen(name => nonBlank(name, "name"))
+        .combine(getValue(form, "email").andThen(emailProperForm))
+        .combine(getValue(form, "password").andThen(passwordCheck))
+        .map(_ => "user registration complete")
+  }
+
+  import cats.syntax.validated._
+  val aValidMeaningOfLife: Validated[List[String], Int] = 42.valid[List[String]]
+  val anError: Validated[String, Int] = "something went wrong".invalid[Int]
+
   def main(args: Array[String]): Unit = {
 
+    val form = Map(
+      "name" -> "daniel",
+      "email" -> "daniel@rockthejvm.com",
+      "password" -> "rockthejvm1!"
+    )
+
+    println(FormValidation.validateForm(form))
   }
 
 }
