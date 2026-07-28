b306cb0493cf9bb2f9de65086954d2a57cb93807
Miguel Morel <memc22@gmail.com>
2026-07-28 18:52:04 -0400

    traversing pt2

--- courses/cats/src/main/scala/part4typeclassses/Traversing.scala
+++ courses/cats/src/main/scala/part4typeclassses/Traversing.scala
@@ -1,6 +1,6 @@
 package part4typeclassses
 
-import cats.{Applicative, Monad}
+import cats.{Applicative, Foldable, Functor, Monad}
 
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
@@ -51,13 +51,59 @@ object Traversing {
   val allTriples = listSequence(List(Vector(1,2), Vector(3,4), Vector(5,6))) // Vector[List[Int]] - all the possible 3-pairs
 
 
+  import cats.instances.option._
+  def filterAsOption(list: List[Int])(predicate: Int => Boolean): Option[List[Int]] =
+//    listTraverse[Option, Int, Int](list)(n => if(predicate(n)) Some(n) else None)
+    listTraverse[Option, Int, Int](list)(n => Some(n).filter(predicate))
+
+  // todo 4
+  val allTrue = filterAsOption(List(2,4,6))(_ % 2 == 0) // Some(2,4,6)
+  val someFalse = filterAsOption(List(1,2,3))(_ % 2 == 0) // None
+
+  import cats.data.Validated
+  import cats.instances.list._ // Semigroup[List] => Applicative[ErrorsOr]
+  type ErrorsOr[T] = Validated[List[String], T]
+  def filterAsValidated(list: List[Int])(predicate: Int => Boolean): ErrorsOr[List[Int]] =
+    listTraverse[ErrorsOr, Int, Int](list) { n =>
+      if(predicate(n)) Validated.valid(n)
+      else Validated.invalid(List(s"predicate for $n failed"))
+    }
+
+  // todo 5
+  val allTrueValidated = filterAsValidated(List(2,4,6))(_ % 2 == 0) // Valid(List(2,4,6))
+  val someFalseValidated = filterAsValidated(List(1,2,3))(_ % 2 == 0) // Invalid(List("predicate for 1 failed", "predicate for 3 failed"))
 
 
+  // mixing in Functor brings its own implementation of map
+  trait MyTraverse[L[_]] extends Foldable[L] with Functor[L] {
+    def traverse[F[_] : Applicative, A, B](list: L[A])(func: A => F[B]): F[L[B]]
+    def Sequence[F[_] : Applicative, A](container: L[F[A]]): F[L[A]] =
+      traverse(container)(identity)
+
+    // todo 6
+    // hint
+    import cats.Id
+//    type Identity[T] = T
+    def map[A, B](wa: L[A])(f: A => B): L[B] =
+      traverse[Id, A, B](wa)(f)
+  }
+
+  import cats.Traverse
+  import cats.instances.future._ // Applicative[Future]
+  val allBandwidthsCats = Traverse[List].traverse(servers)(getBandwidth)
+
+  // extension methods
+  import cats.syntax.traverse._ // sequence + traverse
+  val allBandwidthsCats2 = servers.traverse(getBandwidth)
 
 
   def main(args: Array[String]): Unit = {
     println(allPairs)
     println(allTriples)
+    println(allTrue)
+    println(someFalse)
+    println(allTrueValidated)
+    println(someFalseValidated)
   }
 
 }
