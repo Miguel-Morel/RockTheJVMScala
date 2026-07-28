8ace13d5aee63b0fb985a1601ef5ad5476c50b7e
Miguel Morel <memc22@gmail.com>
2026-07-28 13:01:35 -0400

    semigroupals

---
+++ courses/cats/src/main/scala/part4typeclassses/Semigroupals.scala
@@ -0,0 +1,93 @@
+package part4typeclassses
+
+
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+
+object Semigroupals {
+
+  trait MySemigroupal[F[_]] {
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+  }
+
+  import cats.Semigroupal
+  import cats.instances.option._ // implicit Semigroupal[Option]
+
+  val optionSemigroupal = Semigroupal[Option]
+  val aTupledOption = optionSemigroupal.product(Some(123), Some("a string")) // Some((123, "a string"))
+  val aNoneTupled = optionSemigroupal.product(Some(123), None) // None
+
+  import cats.instances.future._ // implicit Semigroupal[Future]
+  implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+
+  val aTupledFuture = Semigroupal[Future].product(Future("the meaning of life"), Future(42)) // Future("the meaning of life", 42)
+
+  import cats.instances.list._ // Monad[List]
+
+  val aTupledList = Semigroupal[List].product(List(1,2), List("a", "b"))
+
+  // todo: implement
+  import cats.Monad
+  import cats.syntax.functor._ // for map
+  import cats.syntax.flatMap._ // for flatMap
+
+  def productWithMonads[F[_], A, B](fa: F[A], fb: F[B])(implicit monad: Monad[F]): F[(A, B)] =
+    for {
+      a <- fa
+      b <- fb
+    } yield(a, b)
+
+//    monad.flatMap(fa)(a => monad.map(fb)(b => (a, b)))
+
+  // MONADS EXTEND SEMIGROUPALS
+
+  // example: Validated
+  import cats.data.Validated
+  type ErrorsOr[T] = Validated[List[String], T]
+  val validatedSemigroupal = Semigroupal[ErrorsOr] // requires the implicit Semigroup[List[_]]
+
+  val invalidsCombination = validatedSemigroupal.product(
+    Validated.invalid(List("something wrong", "something else wrong")),
+    Validated.invalid(List("this can't be right"))
+  )
+
+  type EitherErrorsOr[T] = Either[List[String], T]
+  import cats.instances.either._ // implicit Monad[Either]
+
+  val eitherSemigroupal = Semigroupal[EitherErrorsOr]
+  val eitherCombination = eitherSemigroupal.product( // in terms of map/flatMap
+    Left(List("something wrong", "something else wrong")),
+    Left(List("this can't be right"))
+  )
+
+//  trait MyMonad[M[_]] extends MySemigroupal[M] {
+//    def pure[A](value: A): M[A]
+//    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+//    def map[A, B](ma: M[A])(f: A => B): M[B] =
+//      flatMap(ma)(x => pure(f(x)))
+//
+//    def product[A, B](fa: M[A], fb: M[B]): M[(A, B)] =
+//      flatMap(fa)(a => map(fb)(b => (a, b)))
+//  }
+
+  // associativity monadic law: m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
+
+  // todo 2: define a Semigroupal[List] which does a zip
+  val zipListSemigroupal: Semigroupal[List] = new Semigroupal[List] {
+    override def product[A, B](listA: List[A], listB: List[B]) = listA.zip(listB)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+//    println(aTupledList)
+
+//    println(invalidsCombination)
+
+//    println(eitherCombination)
+
+    println(zipListSemigroupal.product(List(1,2), List("a", "b")))
+  }
+
+}
