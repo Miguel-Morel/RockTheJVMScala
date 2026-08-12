10d00200998b0217aa33c410e16da73387073741
Miguel Morel <memc22@gmail.com>
2026-08-11 22:34:40 -0400

    IO exercises

--- courses/cats-effect/src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
+++ courses/cats-effect/src/main/scala/com/rockthejvm/part2effects/IOIntroduction.scala
@@ -39,14 +39,97 @@ object IOIntroduction {
   def smallProgram_v2(): IO[Unit] =
     (IO(StdIn.readLine()), IO(StdIn.readLine())).mapN(_ + _).map(println)
 
+  // exercises
+  // 1 - sequence two IOs and take the result of the LAST one
+  // hint: use flatMap
+  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa.flatMap(_ => iob)
+
+  def sequenceTakeLast_v2[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob // andThen
+
+  def sequenceTakeLast_v3[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa >> iob // "andThen" with by-name call
+
+  // 2 - sequence two IOs and take the result of the FIRST one
+  // hint: use flatMap
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(a => iob.map(_ => a))
+
+  def sequenceTakeFirst_v2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob
+
+  // 3 - repeat an IO effect forever
+  // hint: use flatMap + recursion
+  def forever[A](io: IO[A]): IO[A] =
+    io.flatMap(_ => forever(io))
+
+  def forever_v2[A](io: IO[A]): IO[A] =
+    io >> forever_v2(io)
+
+  def forever_v3[A](io: IO[A]): IO[A] =
+    io *> forever_v3(io)
+
+  def forever_v4[A](io: IO[A]): IO[A] =
+    io.foreverM // with tail recursion
+
+  // 4 - convert an IO to a different type
+  // hint: use map
+  def convert[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.map(_ => value)
+
+  def convert_v2[A, B](ioa: IO[A], value: B): IO[B] =
+    ioa.as(value)
+
+  // 5 - discard value inside an IO. just return unit
+  def asUnit[A](ioa: IO[A]): IO[Unit] =
+    ioa.map(_ => ())
+
+  def asUnit_v2[A](ioa: IO[A]): IO[Unit] =
+    ioa.as(()) // discouraged - don't use this
+
+  def asUnit_v3[A](ioa: IO[A]): IO[Unit] =
+    ioa.void // encouraged
+
+  // 6 - fix stack recursion
+  def sum(n: Int): Int =
+    if(n <= 0) 0
+    else n + sum(n - 1)
+
+  def sumIO(n: Int): IO[Int] = {
+    if(n <= 0) IO(0)
+    else for {
+      lastNumber <- IO(n)
+      prevSum <- sumIO(n - 1)
+    } yield prevSum + lastNumber
+  }
+
+  // 7 (hard) - write a fibonacci function that does NOT crash on recursion
+  // hints: use recursion, ignore exponential complexity, use flatMap heavily
+  def fibonacci(n: Int): IO[BigInt] = {
+    if(n < 2) IO(1)
+    else for {
+//      last <- IO(fibonacci(n - 1)).flatMap(x => x)
+//      last <- IO(fibonacci(n - 1)).flatten // same
+      last <- IO.defer(fibonacci(n - 1)) // same
+//      prev <- IO(fibonacci(n - 2)).flatMap(x => x)
+      prev <- IO.defer(fibonacci(n - 2)) // same
+    } yield last + prev
+  }
+
 
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global // "platform"
     // "end of the world"
 //    println(aDelayedIO.unsafeRunSync())
 //    println(smallProgram().unsafeRunSync())
-    println(smallProgram_v2().unsafeRunSync())
+//    println(smallProgram_v2().unsafeRunSync())
 
+//    forever_v3(IO(println("forever"))).unsafeRunSync()
+//    println(sumIO(40000).unsafeRunSync()) // causes stack overflow
+//    sum(40000)
+
+    (1 to 100).foreach(i => println(fibonacci(i).unsafeRunSync()))
   }
 
 }
