94c4de0cbe8c494f5b17b9868c1abc0d62d30c34
Miguel Morel <memc22@gmail.com>
2026-08-14 11:42:20 -0400

    ref

---
+++ courses/cats-effect/src/main/scala/com/rockthejvm/part4coordination/Refs.scala
@@ -0,0 +1,93 @@
+package com.rockthejvm.part4coordination
+
+import cats.effect.{IO, IOApp, Ref}
+import cats.implicits.catsSyntaxParallelSequence1
+import com.rockthejvm.utils.DebugWrapper
+
+object Refs extends IOApp.Simple {
+
+  // ref = purely functional atomic reference
+  val atomicMol: IO[Ref[IO, Int]] = Ref[IO].of(42)
+  val atomicMol_v2: IO[Ref[IO, Int]] = IO.ref(42)
+
+  // modifying is an effect
+//  val increasedMol: IO[IO[Unit]] = atomicMol.map { ref =>
+  val increasedMol: IO[Unit] = atomicMol.flatMap { ref =>
+    ref.set(43) // thread-safe
+  }
+
+  // obtain a value
+  val mol = atomicMol.flatMap { ref =>
+    ref.get // thread-safe
+  }
+
+  val getSetMol: IO[Int] = atomicMol.flatMap { ref =>
+    ref.getAndSet(43) // gets the old value, sets the new one
+  }
+
+  // updating with a function
+  val fMol: IO[Unit] = atomicMol.flatMap { ref =>
+    ref.update(value => value * 10)
+  }
+
+  val updatedMol: IO[Int] = atomicMol.flatMap { ref =>
+    ref.updateAndGet(value => value * 10) // get the new value
+    // can also use getAndUpdate to get the OLD value
+  }
+
+  // modifying with a function returning a different type
+  val modifiedMol: IO[String] = atomicMol.flatMap { ref =>
+    ref.modify(value => (value * 10, s"my current value is $value"))
+  }
+
+  // why do we need this? concurrent + thread-safe reads/writes over shared values, in a purely functional way
+  def demoConcurrentWorkImpure(): IO[Unit] = {
+    var count = 0
+
+    def task(workload: String): IO[Unit] = {
+      val wordCount = workload.split(" ").length
+      for {
+        _ <- IO(s"counting words for '$workload': '$wordCount'").debug
+        newCount <- IO(count + wordCount)
+        _ <- IO(s"new total: $newCount").debug
+        _ <- IO(count += wordCount)
+      } yield ()
+    }
+
+    List("I love cats effect", "this ref thing is useless", "daniel writes a lot of code")
+      .map(task)
+      .parSequence
+      .void
+  }
+
+  /*
+    drawbacks:
+    - hard to read/debug
+    - mix pure/impure code
+    - NOT THREAD-SAFE
+   */
+
+  // ref equivalent
+  def demoConcurrentWorkPure(): IO[Unit] = {
+    def task(workload: String, total: Ref[IO, Int]): IO[Unit] = {
+      val wordCount = workload.split(" ").length
+
+      for {
+        _ <- IO(s"counting words for '$workload': '$wordCount'").debug
+        newCount <- total.updateAndGet(currentCount => currentCount + wordCount)
+        _ <- IO(s"new total: $newCount").debug
+      } yield ()
+    }
+
+    for {
+      initialCount <- Ref[IO].of(0)
+      _ <-  List("I love cats effect", "this ref thing is useless", "daniel writes a lot of code")
+        .map(string => task(string, initialCount))
+        .parSequence
+    } yield ()
+  }
+
+
+  override def run: IO[Unit] = demoConcurrentWorkPure()
+
+}
