6456b44adb370f7a2b9a98ff1b04e8cff2dd9a4a
Miguel Morel <memc22@gmail.com>
2026-08-17 22:03:43 -0400

    NotGiven

---
+++ courses/scala-3-new-features/src/main/scala/com/rockthejvm/part1changes/NotGivens.scala
@@ -0,0 +1,36 @@
+package com.rockthejvm.part1changes
+
+object NotGivens {
+
+  // library code
+  def processLists[A, B](la: List[A], lb: List[B]): List[(A, B)] =
+    for
+      a <- la
+      b <- lb
+    yield(a, b)
+
+  // goal: be able to call processLists ONLY IF the types A and B are different
+  def processSameType[A](la: List[A], lb: List[A]): List[(A, A)] = processLists(la, lb)
+
+  val combinedLists = processLists(List(1,2,3), List("black", "white")) // ok
+  val combinedNumbers = processLists(List(1,2,3), List(4,5)) // not ok - we want this code to NOT compile
+
+  class <:>[A, B]
+  // make the compiler create a <:>[A, A]
+//  given equalType[A]: <:>[A, A] with {}
+  given equalType[A]: <:>[A, A] = new <:>[A, A]
+
+  object DifferentTypes:
+    import scala.util.NotGiven
+    // if compiler cannot find a given T, it will generate a NotGiven[T]
+    def processListsDifferentType[A, B](la: List[A], lb: List[B])(using NotGiven[A <:> B]): List[(A, B)] =
+      processLists(la, lb)
+
+  def main(args: Array[String]): Unit = {
+    import DifferentTypes._
+    val combinedLists = processListsDifferentType(List(1,2,3), List("black", "white")) // ok
+//    val combinedNumbers = processListsDifferentType(List(1,2,3), List(4,5)) // not ok - we want this code to NOT compile
+
+  }
+
+}
