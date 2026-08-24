b131884643b275e73d6f3efc15644b04f2a99f53
Miguel Morel <memc22@gmail.com>
2026-08-24 00:43:45 -0400

    jobs testing

---
+++ courses/typelevel-project/src/test/scala/com/rockthejvm/jobsboard/core/DoobieSpec.scala
@@ -0,0 +1,42 @@
+package com.rockthejvm.jobsboard.core
+
+import cats.effect.{IO, Resource}
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
+import doobie.util.transactor.Transactor
+import org.testcontainers.containers.PostgreSQLContainer
+
+trait DoobieSpec {
+  // simulate a database
+  // docker containers
+  // testContainers
+
+  // to be impolemented by whatever test case interacts with the db
+  val initScript: String
+
+  val postgres: Resource[IO, PostgreSQLContainer[Nothing]] = {
+    val acquire = IO {
+      val container: PostgreSQLContainer[Nothing] = new PostgreSQLContainer("postgres").withInitScript(initScript)
+      container.start()
+      container
+    }
+
+    val release = (container: PostgreSQLContainer[Nothing]) => IO(container.stop())
+
+    Resource.make(acquire)(release)
+  }
+
+  // set up a postgres transactor
+  val transactor: Resource[IO, Transactor[IO]] = for {
+    db <- postgres
+    ce <- ExecutionContexts.fixedThreadPool[IO](1)
+    xa <- HikariTransactor.newHikariTransactor[IO](
+      "org.postgresql.Driver",
+      db.getJdbcUrl(),
+      db.getUsername(),
+      db.getPassword(),
+      ce
+    )
+  } yield xa
+
+}
